90401e47ee15805e3f97367a5d0deaabcd82b43a
Austin Pratt <hargis32@gmail.com>
2022-02-28 11:28:59 -0600

    updated cartItem index.js to use react-redux

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { useStoreContext } from "../../utils/GlobalState";
+// import { useStoreContext } from "../../utils/GlobalState";
+// no need to import useSelector, 
+import { useDispatch } from 'react-redux';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
+
 const CartItem = ({ item }) => {
 
-  const [, dispatch] = useStoreContext();
+  // const [, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
 
   const removeFromCart = item => {
     dispatch({
