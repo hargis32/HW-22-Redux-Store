d91875e6bf49b4b53777a8ea022e74c7395f14c8
Austin Pratt <hargis32@gmail.com>
2022-02-28 10:36:30 -0600

    added store.js per miniproject readme

---
+++ client/src/utils/store.js
@@ -0,0 +1,9 @@
+// Creates a Redux store that holds the state of the app. Only one store should exist.
+import { createStore } from 'redux';
+
+// Importing the reducer file that is mostly unchanged
+import reducers from './reducers';
+
+export default createStore(reducers);
+
+// pulled from miniproject
